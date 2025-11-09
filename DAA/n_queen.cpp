9a8de9faf986b8a12d35311e0ce669f278fbb4e7
yashcodesfor3 <yashjadhav1509.work@gmail.com>
2025-11-09 13:13:47 +0530

    Create n_queen.cpp

---
+++ DAA/n_queen.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+#define N 8   // chessboard size
+
+bool isSafe(int board[N][N], int row, int col) {
+    for (int i = 0; i < col; i++)
+        if (board[row][i]) return false;
+
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        if (board[i][j]) return false;
+
+    for (int i = row, j = col; j >= 0 && i < N; i++, j--)
+        if (board[i][j]) return false;
+
+    return true;
+}
+
+bool solveNQ(int board[N][N], int col) {
+    if (col >= N) return true;
+
+    for (int i = 0; i < N; i++) {
+        if (isSafe(board, i, col)) {
+            board[i][col] = 1;
+
+            if (solveNQ(board, col + 1)) return true;
+
+            board[i][col] = 0; // backtrack
+        }
+    }
+    return false;
+}
+
+void printSolution(int board[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << (board[i][j] ? "Q " : ". ");
+        cout << endl;
+    }
+}
+
+int main() {
+    int board[N][N] = {0};
+    if (!solveNQ(board, 0))
+        cout << "Solution does not exist";
+    else
+        printSolution(board);
+    return 0;
+}
